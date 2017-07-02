435e015ce9dc28952bef9104acc222f44d793304
andrey.korlyuk-github <andrey.korlyuk@gmail.com>
2017-07-02 22:00:51 +0300

    add DB tests

---
+++ expensemanager/src/test/java/com/engagetech/DatabaseTest.java
@@ -0,0 +1,29 @@
+package com.engagetech;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import com.engagetech.expensemanager.core.Expense;
+import com.engagetech.expensemanager.core.ExpenseDAO;
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class DatabaseTest extends JdbiUnitTest{
+
+    private ExpenseDAO expenseDAO;
+
+
+    @Test
+    public void createsExpense() {
+        Expense expense = new Expense(0L, new DateTime(2017, 07, 02, 03,00).toDate(), BigDecimal.valueOf(200), "Because");
+
+        expenseDAO.insert(expense);
+
+        assertThat(expenseDAO.getAll().contains(expense));
+    }
+
+    @Override
+    protected void setUpDataAccessObjects() {
+        expenseDAO = getDbi().onDemand(ExpenseDAO.class);
+    }
+}
